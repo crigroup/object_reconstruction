55a26e873c1884fbd6cc47c8d465fcea090035bd
fsuarez6 <fsuarez6@gmail.com>
2016-08-26 16:05:50 +0800

    Change parameters for the camera_pipeline, make sure the msg headers are consistent and update package dependencies

--- scripts/capture.py
+++ scripts/capture.py
@@ -1,145 +1,77 @@
 #!/usr/bin/env python
 import os
 import sys
+import copy
 import math
 import rospy
+import criros
 import argparse
 import textwrap
 import numpy as np
 import dynamic_reconfigure.client
 # OpenCV
 import cv2
-from cv_bridge import CvBridge, CvBridgeError
-from image_geometry import StereoCameraModel
 # PCL
 import pcl
-import ros_numpy
 from ros_numpy.point_cloud2 import get_xyz_points, pointcloud2_to_array
+# Python ensenso snatcher
+from ensenso.snatcher import Snatcher
 # Messages
+from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import (
   Image, 
   PointCloud2
 )
 
 
-class CaptureServer(object):
+class CaptureServer(Snatcher):
   def __init__(self, options):
+    super(CaptureServer, self).__init__()
     # Config stuff
-    np.set_printoptions(precision=4, suppress=True)
-    self.bridge = CvBridge()
+    np.set_printoptions(precision=6, suppress=True)
     self.nviews = options.nviews
-    self.exposure_time = 1.5
     # Setup publishers and subscribers
-    self.reset_snapshots()
-    rospy.Subscriber('left/image_raw', Image, self.cb_raw_left)
-    rospy.Subscriber('right/image_raw', Image, self.cb_raw_right)
-    rospy.Subscriber('left/image_rect', Image, self.cb_rect_left)
-    rospy.Subscriber('right/image_rect', Image, self.cb_rect_right)
-    rospy.Subscriber('depth/points', PointCloud2, self.cb_point_cloud)
-    self.dbg_pub = rospy.Publisher('capture/debug', Image, queue_size=1)
-    # Camera configuration client
-    self.dynclient = dynamic_reconfigure.client.Client('ensenso_driver', timeout=30, config_callback=self.cb_dynresponse)
+    self.dbg_pub = rospy.Publisher('debug/image_color', Image, queue_size=1)
+    rospy.Subscriber('pattern/pose', PoseStamped, self.cb_pattern_pose)
     rospy.on_shutdown(self.on_shutdown)
   
-  def cb_dynresponse(self, config):
-    pass
+  def cb_pattern_pose(self, msg):
+    self.pattern_pose = criros.conversions.from_pose(msg.pose)
+    self.headers['pattern_pose'] = copy.deepcopy(msg.header)
   
-  def cb_point_cloud(self, msg):
-    try:
-      self.point_cloud = msg
-    except:
-      self.point_cloud = None
-  
-  def cb_raw_left(self, msg):
-    try:
-      self.raw_left = self.bridge.imgmsg_to_cv2(msg, 'mono8')
-    except:
-      rospy.logdebug('Failed to process left image')
-      self.raw_left = None
-  
-  def cb_raw_right(self, msg):
-    try:
-      self.raw_right = self.bridge.imgmsg_to_cv2(msg, 'mono8')
-    except:
-      rospy.logdebug('Failed to process right image')
-      self.raw_right = None
-  
-  def cb_rect_left(self, msg):
-    try:
-      self.rect_left = self.bridge.imgmsg_to_cv2(msg, 'mono8')
-    except:
-      rospy.logdebug('Failed to process left image')
-      self.rect_left = None
-  
-  def cb_rect_right(self, msg):
-    try:
-      self.rect_right = self.bridge.imgmsg_to_cv2(msg, 'mono8')
-    except:
-      rospy.logdebug('Failed to process right image')
-      self.rect_right = None
-  
-  def cloud_available(self):
-    return (self.point_cloud is not None)
-  
-  def images_available(self):
-    raw_available = (self.raw_left is not None) and (self.raw_right is not None)
-    rect_available =(self.rect_left is not None) and (self.rect_right is not None)
-    return raw_available and rect_available
+  def good_msgs_headers(self):
+    required_keys = ['raw_left', 'raw_right', 'rect_left', 'rect_right', 'pattern_pose']
+    if not criros.utils.has_keys(self.headers, required_keys):
+      return False
+    stamps = []
+    frames = []
+    for header in self.headers.values():
+      stamps.append( header.stamp.to_sec() )
+      frames.append( header.frame_id )
+    all_frames_equal = frames.count(frames[0]) == len(frames)
+    all_stamps_equal = np.all([ np.isclose(stamps[0],x,atol=1e-3) for x in stamps ])
+    return (all_frames_equal and all_stamps_equal)
   
   def execute(self):
     # Start streaming
-    self.dynclient.update_configuration({'Cloud':True, 'Images':True, 'Projector':False})
-    rospy.sleep(self.exposure_time)
+    self.enable_lights(projector=True, frontlight=False)
+    self.enable_streaming(cloud=False, images=True)
+    rospy.sleep(2.0)
     taken = 0
     while taken < self.nviews and not rospy.is_shutdown():
       self.reset_snapshots()
-      while not (self.cloud_available() and self.images_available()):
-        rospy.sleep(0.01)
-        if rospy.is_shutdown():
-          return
-      # Locate the A3 page
-      image = self.raw_left
-      smooth = cv2.medianBlur(image, 7)
-      dbg_image = cv2.cvtColor(smooth, cv2.COLOR_GRAY2RGB)
-      # Contours
-      (thresh, bw) = cv2.threshold(smooth, 50, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-      _, contours, hierarchy = cv2.findContours(bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-      # Find the biggest rectangle in the image
-      max_area = -float('inf')
-      rect = None
-      for i,cnt in enumerate(contours):
-        # Check only promising contours
-        epsilon = 0.05*cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, epsilon, True)
-        if len(approx) == 4:
-          cnt_area = cv2.contourArea(cnt)
-          if cnt_area > max_area:
-            max_area = cnt_area
-            rect = cv2.boundingRect(cnt)
-      if rect is None:
+      self.take_snapshot(exposure_time=0.001, success_fn=self.has_images)
+      if not self.good_msgs_headers():
+        rospy.logdebug('The headers of the images and pattern must be equal')
         continue
-      x,y,w,h = rect
-      cv2.rectangle(dbg_image, (x,y), (x+w,y+h), (0,255,0), 4)
-      # Crop the image
-      roi = image[y:y+h, x:x+w]
-      success, centers = cv2.findCirclesGrid(roi, (5, 3), flags=cv2.CALIB_CB_ASYMMETRIC_GRID)
-      if centers is None:
-        continue
-      for center in np.uint16( np.squeeze(centers) ):
-        cv2.circle(dbg_image, tuple(center), 5, (255,0,0), 10)
-      # Publish debugging image
-      self.dbg_pub.publish( self.bridge.cv2_to_imgmsg(dbg_image, 'rgb8') )
+      #~ # Debugging image
+      #~ dbg_image = cv2.cvtColor(self.rect_left, cv2.COLOR_GRAY2RGB)
+      #~ self.dbg_pub.publish( self.bridge.cv2_to_imgmsg(dbg_image, 'rgb8') )
   
   def on_shutdown(self):
     # Stop streaming
-    self.dynclient.update_configuration({'Cloud':False, 'Images':False, 'Projector':False})
-  
-  def reset_snapshots(self):
-    self.point_cloud = None
-    self.raw_left = None
-    self.raw_right = None
-    self.rect_left = None
-    self.rect_right = None
+    self.enable_lights(projector=False, frontlight=False)
+    self.enable_streaming(cloud=False, images=False)
 
 
 def parse_args():
@@ -153,12 +85,12 @@ object sparsely, depending on the angle_thresh setting."""),
                       default='',
                       help='A bagfile to write to.')
   parser.add_argument('-a', '--angle_thresh', metavar='RADIANS', dest='angle_thresh', type=float,
-                      default= 10 * math.pi / 180, #10 degrees
+                      default= 10 * math.pi / 180,
                       help='The delta angular threshold in pose.'
-                           'Frames will not be recorded unless they are not closer to any other pose by this amount. default(%(default)s)')
+                           'Frames will not be recorded unless they are not closer to any other pose by this amount. default=%(default).2f')
   parser.add_argument('-n', '--nviews', metavar='NVIEWS', dest='nviews', type=int,
                       default=36,
-                      help='Number of desired views. default(%(default)s)')
+                      help='Number of desired views. default=%(default)d')
   parser.add_argument('-p', '--preview', dest='preview', action='store_true',
                       default=False, help='Preview the pose estimator.')
   parser.add_argument('--debug', action='store_true',     
